48cc00fefd881bc06188ce5cfc4aa1f6e95e7b2b
ralvztech <info@ralvz.com>
2023-02-21 09:52:15 -0800

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
  *
  *   betty style doc for function amin goes there
  *
- *   Return: 0 (Success)
+ *   Return: Always 0 (Success)
  */
 
 int main(void)
@@ -17,17 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else
-	{
-		printf("%d is negative\n", n);
-	}
+		rintf("%d is negative\n", n);
 	return (0);
 }
 
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print single digit number of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	in c;
+
+	c = a;
+	while
+		(c < 10) {
+			putchar('c';
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
