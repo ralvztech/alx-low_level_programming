d4a09cda181ddebf557f38fa4da88af05f7e5023
ralvztech <info@ralvz.com>
2023-02-27 09:18:49 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - A program to print sign of a number
+ * @n: THe number to be checked
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(40);
+	return (0);
+	}
+}
+
