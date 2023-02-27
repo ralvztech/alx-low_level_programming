654c4fe03fca2f4c142b7bc12ca2e3133aab5fcb
ralvztech <info@ralvz.com>
2023-02-26 20:46:34 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - A program to check for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 1 for anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
