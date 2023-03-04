e51a363ea0bfb08bc0685b8ce72eda68b9da0a9a
ralvztech <info@ralvz.com>
2023-03-04 14:48:35 -0800

    fix

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string plus newline
+ * @str: string to be printed
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i};
+		i++;
+	}
+	_putcnar('\n');
+}
