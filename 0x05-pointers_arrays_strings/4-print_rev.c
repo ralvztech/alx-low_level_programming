3f87e426b074ea26483a7909496881c001c74ca1
ralvztech <info@ralvz.com>
2023-03-04 15:16:48 -0800

    fis

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: param
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 1 - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
