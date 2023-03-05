a2a0f8fa51ef5bebaa435b51ff1ae5ff891b3066
ralvztech <info@ralvz.com>
2023-03-05 13:37:24 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: param
+ * @dest: param 2
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, charn*src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != 0)
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
+
