a1ee78369aed67abbbcd4bc470d142223d8b79c2
ralvztech <info@ralvz.com>
2023-09-04 09:29:37 -0700

    compliling the libmy static lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,7 +1,8 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes the characiter c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
