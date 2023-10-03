351f531988ef0b775615e9cce35d13acbc26a2a3
ralvztech <info@ralvz.com>
2023-10-03 03:08:44 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,36 @@
 #include "main.h"
-#define FAIL 0
-#define CLOSE free(buff)
 
 /**
- * read_textfile - reads the content of a file and prints it to
- * POSIX standart output
- * @filename: file nin the current pwd
- * @letters: amount of letters it should read and print
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: the actual number of letters it could print
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int reading;
-	char *buff = malloc(sizeof(char) * (letters + 1));
-	ssize_t cnt_write = 0;
-	unsigned int i;
+	int fd;
+	int s, t;
+	char *buf;
 
-	if (filename == NULL || buff == NULL)
-		return (FAIL);
+	if (!filename)
+		return (0);
 
-	for (i = 0; i <= letters; i++)
-		buff[i] = '\0';
+	fd = open(filename, O_RDONLY);
 
-	reading  = open(filename, O_RDONLY);
-	if (reading == -1)
-	{
-		CLOSE;
-		return (FAIL);
-	}
+	if (fd == -1)
+		return (0);
 
-	if (read(reading, buff, letters) == -1)
-	{
-		CLOSE;
-		return (FAIL);
-	}
-	for (i = 0; buff[i] != '\0'; i++)
-		;
-	cnt_write = write(STDOUT_FILENO, buff, i);
-	if (cnt_write == -1)
-	{
-		CLOSE;
-		return (FAIL);
-	}
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
+	s = read(fd, buf, letters);
+	t = write(STDOUT_FILENO, buf, s);
 
-	close(reading);
-	CLOSE;
+	close(fd);
 
-	return (cnt_write);
-}
+	free(buf);
+
+	return (t);
+}}
